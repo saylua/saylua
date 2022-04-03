35fb179368a30ec1e8f50d30596cc22e6991bded
Tiffany Zhang <tiff@Tiffanys-MacBook-Pro.local>
2022-04-02 20:29:33 -0700

    Saylua-ify some placeholder content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,11 +6,8 @@ export default function App() {
   const [count, setCount] = useState(0);
   return (
     <div className={styles.container}>
-      <h2>React minimal starter kit</h2>
-      <p>
-        This is a simple starter kit to help you bootstrap your React project
-        with few frills.
-      </p>
+      <h2>Saylua</h2>
+      <p>The starting point for a new virtual pet game.</p>
       <button type="button" onClick={() => setCount(count + 1)}>
         Increase cat count: {count}
       </button>
@@ -19,14 +16,10 @@ export default function App() {
   );
 }
 
-function CatCounter({count}: {count: number}) {
-  let cats: Array<String> = [];
+function CatCounter({ count }: { count: number }) {
+  const cats: Array<String> = [];
   for (let i = 0; i < count; i++) {
     cats.push('🐱');
   }
-  return (
-    <>
-      {cats}
-    </>
-  );
+  return <>{cats}</>;
 }
